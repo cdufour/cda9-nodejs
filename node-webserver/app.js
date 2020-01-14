35b893fb387202712fda92c4cbc81353d475ade9
cdufour <opusidea@gmail.com>
2020-01-14 11:11:51 +0100

    am break

--- node-webserver/app.js
+++ node-webserver/app.js
@@ -1,7 +1,38 @@
 const http = require('http');
+const fs = require('fs');
 
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.write('Fine della prima giornata');
-    res.end();
-}).listen(5000);
+const resBody = "Le serveur a répondu à la requête";
+
+http
+.createServer((req, res) => {
+
+    if (req.url == '/') {
+        res.writeHead(200, {'Content-Type':'text/plain'});
+        res.write('Homepage');
+        res.end();
+    } else if (req.url.indexOf('login') != -1) {
+        res.writeHead(200, {'Content-Type':'text/plain'});
+        res.write('login string found in url');
+        res.end();
+    } else if (req.url == '/horaires') {
+        res.writeHead(200, {
+            'Content-Type':'text/html',
+            'X-Header-Test':'Info perso'
+        });
+        fs.readFile('./static/horaires.html', (err, data) => {
+            if (err) console.log(err);
+            res.end(data);
+        });
+    } else if (req.url == '/styles.css') {
+        res.writeHead(200, {'Content-Type':'text/css'});
+        fs.readFile('./static/styles.css', (err, data) => {
+            if (err) console.log(err);
+            res.end(data);
+        });
+    } else {
+        res.end();
+    }
+
+    //res.end();
+})
+.listen(5000);
